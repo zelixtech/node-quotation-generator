16c29f4eeca825e6801f34afb2bed3590c9f827b
Smit Vasoya <smitvtridhyatech@gmail.com>
2023-01-17 22:26:49 +0530

    completed invoice pdf generator

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
-const homeRoutes = require('./routes/home-routes');
-const bodyParser = require('body-parser');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const homeRoutes = require("./routes/home-routes");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
-
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 const app = express();
+app.use(express.json());
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/docs', express.static(path.join(__dirname, 'docs')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/docs", express.static(path.join(__dirname, "docs")));
 app.use(homeRoutes.routes);
 
-
-app.listen(8000, () => console.log('App is listening on url http://localhost:8000'));
+app.listen(8000, () =>
+  console.log("App is listening on url http://localhost:8000")
+);
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const { homeview, generatePdf } = require('../controllers/homeController');
+const express = require("express");
+const {
+  homeview,
+  generatePdf,
+  generateInvoicePdf,
+} = require("../controllers/homeController");
 
 const router = express.Router();
 
-router.get('/', homeview);
+router.get("/", homeview);
 // router.get('/download', generatePdf);
-router.post('/download', generatePdf);
+router.post("/download", generatePdf);
+router.post("/createInvoice", generateInvoicePdf);
 
 module.exports = {
-    routes: router
-}
+  routes: router,
+};
